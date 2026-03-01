c9d889ed3f8cd6233fe2edca5442df046043f751
Ekwinder Singh <ekwindersaini@gmail.com>
2026-03-01 18:12:31 -0500

    Use v0.1 release checksum

--- Casks/screenpath.rb
+++ Casks/screenpath.rb
@@ -1,6 +1,6 @@
 cask "screenpath" do
   version "0.1"
-  sha256 :no_check
+  sha256 "473b185ed49efc82eaa825234c200c749f2fcae608ff171424108a84700c98d8"
 
   url "https://github.com/Ekwinder/screenpath/releases/download/v#{version}/ScreenPath.app.zip"
   name "ScreenPath"
